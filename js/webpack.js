d64fd80f36c35250e4af60cf27dff2a15d09d221
shuric-git <ulme@rambler.ru>
2021-11-17 20:25:38 +0300

    make button GROW

--- js/webpack.js
+++ js/webpack.js
@@ -1,14 +1,25 @@
 'use strict'
-
+let growWidth
+let growHeight
+// let buttonWidth
+let buttonWidth = getComputedStyle(button).width
+let buttonHeight = getComputedStyle(button).height
 
 button.addEventListener( 'click', function() {
     button.style.color = 'violet'
-    i++
+    ++i
     button.innerHTML = i
-    if ( i <= 10 ) {
-        let test
-        let buttonWidth = getComputedStyle(button).width
-        test = toString(Number(buttonWidth) + i) + 'px'
-        console.log(test)
-    } 
+    if ( i < 10 ) {
+        let widthNumber = buttonWidth.split('px')
+        growWidth = Number(widthNumber[0]) + (i * 4) + 'px'
+        button.style.width = growWidth
+        let heightNumber = buttonHeight.split('px')
+        growHeight = Number(heightNumber[0]) + (i * 4) + 'px'
+        button.style.height = growHeight
+        console.log(i)
+    } else if ( i = 0 ) {
+        button.style.width = '200px'
+    } else if ( i = 10 ) {
+        i = 0
+    }
 })
