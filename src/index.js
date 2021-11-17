afa8135eafdc2404678f63102299d8bb5279aaf2
shuric-git <ulme@rambler.ru>
2021-11-17 23:37:11 +0300

    first build, not working -_-

---
+++ src/index.js
@@ -0,0 +1,9 @@
+'use strict'
+const container = document.querySelector('.container')
+button = document.createElement('button')
+let i = 0; 
+
+container.append(button)
+button.className = 'button'
+import button from './webpack'
+
